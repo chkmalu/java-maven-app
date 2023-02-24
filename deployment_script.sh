0b8492a46b69856de76a9d75d2eee4b9a88b9013
malu <chkmalu@gmail.com>
2023-02-24 13:40:55 +0100

    updated

--- deployment_script.sh
+++ deployment_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export DOCKER_USER=$1
 export DOCKER_PASS=$2
-echo $DOCKER_PASS | docker loging -u $DOCKER_USER --pass-stdin 
+echo $DOCKER_PASS | docker login -u $DOCKER_USER --pass-stdin 
 docker-compose -f compose.yaml up
