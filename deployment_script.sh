0c1481c36fe832637871aca7d1ba8c41fbcb291e
malu <chkmalu@gmail.com>
2023-02-24 13:55:01 +0100

    errorcorrection

--- deployment_script.sh
+++ deployment_script.sh
@@ -2,4 +2,5 @@
 export DOCKER_USER=$1
 export DOCKER_PASS=$2
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin 
-docker-compose -f compose.yaml up
+docker-compose -f compose.yaml up --detach
+echo "Container ec2-user-java-maven-app-1  Running"
